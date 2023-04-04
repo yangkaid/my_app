2bf9be1e721e73040eec24428c09e24713f6e4df
yangkaid <1376086260@qq.com>
2023-04-04 18:14:54 +0800

    init

---
+++ lib/home.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'web_view_container.dart';
+
+class Home extends StatelessWidget {
+  static final links = ['http://192.168.220.49:5173/'];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+            child: SingleChildScrollView(
+                child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: links.map((link) => _urlButton(context, link)).toList(),
+    ))));
+  }
+
+  Widget _urlButton(BuildContext context, String url) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        child: MaterialButton(
+          color: Theme.of(context).primaryColor,
+          padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 15.0),
+          child: Text(url),
+          onPressed: () => _handleURLButtonPress(context, url),
+        ));
+  }
+
+  void _handleURLButtonPress(BuildContext context, String url) {
+    Navigator.push(context,
+        MaterialPageRoute(builder: (context) => WebViewContainer(url)));
+  }
+}
